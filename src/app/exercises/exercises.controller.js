45c579c7a9f1a41a34d1d08a9cbd51da10484f09
Tekisha Hammock <tekisha.hammock@gmail.com>
2016-06-21 15:56:15 -0500

    set up button functionality for exercises, grabbing inputs

--- src/app/exercises/exercises.controller.js
+++ src/app/exercises/exercises.controller.js
@@ -2,19 +2,51 @@ angular.module("app")
   .controller("ExercisesCtrl", function($scope, $routeParams, ExercisesFactory){
     const currentQuestion = parseInt(($routeParams.question).replace("q", "")) - 1;
     $scope.exercise = ExercisesFactory.all()[currentQuestion];
+    $scope.exercise.next = "";
+    const answerList = document.getElementsByClassName("answers");
 
-    // Later rename to signify change to next question
-    $scope.evaluateAnswer = () => {
+    // Toggles selected class on Multiple Choice
+    $scope.userSelectedAnswer = () => {
+      for (let i = 0; i < answerList.length; i++) {
+        if (answerList.item(i).classList.contains("selected")) {
+          answerList.item(i).classList.remove("selected");
+        }
+      }
+      $scope.selectedAnswer = event.target;
+      $scope.selectedAnswer.classList.add("selected");
+    };
+
+    // Evaluates user input for correct answer
+    $scope.evaluateMultipleChoice = () => {
+      if ($scope.selectedAnswer.value === "true") {
+        $scope.correct = true;
+        $scope.incorrect = false;
+      } else if ($scope.selectedAnswer.value === "false") {
+        $scope.incorrect = true;
+        $scope.correct = false;
+      } else {
+        return;
+      }
+    };
+
+    // Evaluates user input for correct answer
+    $scope.evaluateFillBlank = () => {
+      if (parseInt($scope.userInput) === $scope.exercise.answer) {
+        $scope.correct = true;
+        $scope.incorrect = false;
+      } else {
+        $scope.incorrect = true;
+        $scope.correct = false;
+      }
+    };
+
+    // Pulls user input to affect project appearance
+    $scope.evaluateUserPreference = () => {
+      console.log($scope.selectedAnswer.value);
+    };
+
+    // Moves to the next question in the exercise list
+    $scope.nextExercise = () => {
       console.log("Next question!");
     };
   });
-
-
-// find the banana
-// var array = [{name: 'apple'}, {name: 'banana'}, {name: 'carrot'}]
-// for (var i = 0; i < array.length); i++) {
-//   if (array[i].name = 'banana') {
-//       return array[i]
-//   }
-// }
-// {name: 'banana'}
